e5fa8e6575e4a57d24e26a5d64af2bb21d1bc454
stanmozolevskiy <stanmozolevskiy@gmail.com>
2023-05-14 08:53:11 -0400

    Add project files.

---
+++ DataBus/Controllers/EcoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DataBus.Controllers
+{
+    [Route("[controller]"), ApiController]
+    public class EcoController : ControllerBase
+    {
+        [HttpGet("{input}")]
+        public IActionResult Eco(string input) => Ok(input);
+
+    }
+}
---
+++ DataBus/Program.cs
@@ -0,0 +1,38 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+WebApplication app = builder.Build();
+
+app.Use(async (context, next) =>
+{
+    context.Request.EnableBuffering();
+    await next();
+});
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller}/{action=Index}/{id?}");
+});
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
