ade6b53480e4a42e35050cd2d1bcd40493fc526d
stanmozolevskiy <stanmozolevskiy@gmail.com>
2023-05-17 16:14:46 -0400

    delete method for User controller

--- DataBus/Controllers/UserController.cs
+++ DataBus/Controllers/UserController.cs
@@ -3,18 +3,18 @@ using System.Xml.Linq;
 
 namespace DataBus.Controllers
 {
-    [Route("[controller]"), ApiController]
+    [Route("[controller]"), ApiController, Produces("application/xml")]
     public class UserController: ControllerBase
     {
         [HttpPost("GetUserById"), Produces("application/xml")]
         public IActionResult GetUser(XElement user)
         {
+           
             string userId = user.Element("Id").Value;
-
             return Ok(getUserById(int.Parse(userId)));
         }
 
-        [HttpPost("CreateUser"), Produces("application/xml")]
+        [HttpPost("CreateUser")]
         public IActionResult CreateUser([FromBody] XElement user)
         {
 
@@ -25,7 +25,7 @@ namespace DataBus.Controllers
             if (isExist)
                 return Ok("The user already in the Database");
 
-            XElement users = XDocument.Load(@"/app/heroku_output/UserLIst.xml").Root;
+            XElement users = XDocument.Load(@"UserLIst.xml").Root;
 
             // if not create user 
             users.Add(user);
@@ -36,10 +36,29 @@ namespace DataBus.Controllers
             return Ok("User successfuly added");
         }
 
-        [HttpDelete]
+        [HttpPost("DeleteUser")]
         public IActionResult DeleteUser([FromBody] XElement user)
         {
-            return Ok();
+            string userId = user.Element("Id").Value;
+            var isExist = doesUserexist(int.Parse(userId));
+
+            // check if user exists 
+            if (!isExist)
+                return Ok("The user is not found");
+
+            // get the list of all users
+            XElement users = XDocument.Load(@"UserLIst.xml").Root;
+
+            // find existing user
+            XElement existingUser = users.Elements("User").ToList().FirstOrDefault(x => x.Element("Id").Value == userId);
+
+            // remove the node with existing user
+            existingUser.Remove();
+
+            // write back to the file 
+            users.Save("UserList.xml");
+
+            return Ok("User has been deleted");
         }
 
         [HttpPut]
