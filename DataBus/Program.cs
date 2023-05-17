484f7bb92867447832cf970ad5e1b149b109d64c
stanmozolevskiy <stanmozolevskiy@gmail.com>
2023-05-16 20:16:47 -0400

    fixing the heroku problem

--- DataBus/Program.cs
+++ DataBus/Program.cs
@@ -24,6 +24,9 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
